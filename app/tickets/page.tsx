4deea57a9ff771afb88dccb884e2f7f157298235
foziaShareen <foziashareen1@gmail.com>
2025-02-05 20:53:39 +0500

    button added go to new ticket

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -2,6 +2,8 @@ import DataTable from '@/components/DataTable'
 import React from 'react'
 
 import { PrismaClient } from '@prisma/client'
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
 
 const prisma = new PrismaClient()
 
@@ -9,6 +11,7 @@ const Tickets = async() => {
     const tickets = await prisma.ticket.findMany()
     return (
         <div>
+            <Link href="/tickets/new" className={buttonVariants({variant:"default"})}>New Ticket</Link>
         <DataTable tickets={tickets} />
         </div>
     )
